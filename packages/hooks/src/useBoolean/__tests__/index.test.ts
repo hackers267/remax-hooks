246f9f4e7b356b775a4609f9ec5083451386bd38
silence_zhpf <silence_zhpf@aliyun.com>
2020-07-15 11:47:03 +0800

    :test 完善useBoolean的单元测试

--- packages/hooks/src/useBoolean/__tests__/index.test.ts
+++ packages/hooks/src/useBoolean/__tests__/index.test.ts
@@ -7,4 +7,20 @@ describe('useBoolean', () => {
   it('should be defined', function () {
     expect(useBoolean).toBeDefined();
   });
+  it('test on methods', async () => {
+    const {result} = setUp();
+    expect(result.current[0]).toBeFalsy();
+    act(()=>{
+      result.current[1].setTrue();
+    })
+    expect(result.current[0]).toBeTruthy();
+    act(()=>{
+      result.current[1].setFalse();
+    })
+    expect(result.current[0]).toBeFalsy();
+  });
+  it('test on optional', async ()=>{
+    const hook = setUp(true);
+    expect(hook.result.current[0]).toBeTruthy();
+  });
 });
