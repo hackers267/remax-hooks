cbc9cc60ca31141474cd9cb2fe5ab930b5a08010
silence_zhpf <silence_zhpf@aliyun.com>
2020-09-27 21:25:35 +0800

    ✨ feature: 修改useBoolean文档

--- packages/hooks/src/useBoolean/demos/Demo.tsx
+++ packages/hooks/src/useBoolean/demos/Demo.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { View, Button } from 'remax/one';
+import { View, Button, Text } from 'remax/one';
 import { useBoolean } from 'remax-hook';
+import './index.less';
 
 export default () => {
   const [state, { setTrue, setFalse }] = useBoolean();
   return (
     <View>
-      <View>state:{state ? 'true' : 'false'}</View>
-      <Button onTap={() => setTrue()}>Show</Button>
-      <Button onTap={() => setFalse()}>False</Button>
+      <View className={'state_wrapper'}>
+        state:<Text className={'state'}>{state ? 'true' : 'false'}</Text>
+      </View>
+      <View className={'button_wrapper'}>
+        <Button className={'button'} onTap={() => setTrue()}>
+          Show
+        </Button>
+        <Button className={'button'} onTap={() => setFalse()}>
+          False
+        </Button>
+      </View>
     </View>
   );
 };
