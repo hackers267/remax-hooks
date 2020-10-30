a77af2a8dfce46399cf0f2b2e19208b282869650
silence_zhpf <silence_zhpf@aliyun.com>
2020-10-30 15:10:53 +0800

    build(jest): :wrench: 修改配置
    jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    preset: 'ts-jest/presets/js-with-ts',
-    clearMocks:true,
-    globals: {
-        'ts-jest':{
-            tsConfig:'tsconfig.json'
-        }
-    }
-}
+  preset: 'ts-jest/presets/js-with-ts',
+  clearMocks: true,
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
